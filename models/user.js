3ba28580be429b962a4e416272ecb0a32f8cdb5f
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2026-03-12 13:06:09 +0700

    add documentation for user routes

--- models/user.js
+++ models/user.js
@@ -10,8 +10,43 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Email tidak boleh null"
+          },
+          notEmpty: {
+            msg: "Email tidak boleh kosong"
+          },
+          isEmail: {
+            msg: "Format email tidak valid"
+          },
+          isString(value) {
+            if (typeof value !== "string") {
+              throw new Error("Email harus berupa string");
+            }
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Password tidak boleh null"
+          },
+          notEmpty: {
+            msg: "Password tidak boleh kosong"
+          },
+          isString(value) {
+            if (typeof value !== "string") {
+              throw new Error("Password harus berupa string");
+            }
+          }
+        }
+      }
     },
     {
       sequelize,
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,17 +1,75 @@
 const { UserController } = require("../controllers");
 const userRouter = require("express").Router();
 
-// CRUD Basic
-userRouter.get("/", UserController.getUsers);
-userRouter.post("/create", UserController.add);
-userRouter.delete("/delete/:id", UserController.delete);
-userRouter.put("/edit/:id", UserController.edit);
+// // CRUD Basic
+// userRouter.get("/", UserController.getUsers);
+// userRouter.post("/create", UserController.add);
+// userRouter.delete("/delete/:id", UserController.delete);
+// userRouter.put("/edit/:id", UserController.edit);
 
-// More Routes
-userRouter.get("/search", UserController.search);
-userRouter.get("/details/:id", UserController.getUserById);
+// // More Routes
+// userRouter.get("/search", UserController.search);
+// userRouter.get("/details/:id", UserController.getUserById);
 
 // Login (Authentication) dan Register
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User authentication API
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: admin@mail.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid email or password
+ */
 userRouter.post("/login", UserController.login);
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: ignatius@mail.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Validation error
+ */
 userRouter.post("/register", UserController.register);
 module.exports = userRouter;
